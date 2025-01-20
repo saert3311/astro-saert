e23b74f8f6a6ba68081f28330b5ca50fdf9ad0e7
Jose Duarte <saert@outlook.com>
2025-01-20 12:41:49 +0100

    Welcome translated

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -10,10 +10,18 @@ export const ui = {
         'nav.tools': 'Tools',
         'nav.projects': 'Projects',
         'nav.experience': 'Experience',
+        'welcome.avaliable': 'Avaliable to work',
+        'welcome.hello': "Hello, I'am Jose Duarte (saert)",
+        'welcome.web': 'Web developer',
+        'welcome.enthusiast': 'enthusiastic about technology and its impact on daily life, committed to creating innovative web solutions.',
     },
     es: {
         'nav.tools': 'Herramientas',
         'nav.projects': 'Proyectos',
         'nav.experience': 'Experiencia',
+        'welcome.avaliable': 'Disponible para trabajar',
+        'welcome.hello': "Hola, soy Jose Duarte (saert)",
+        'welcome.web': 'Web developer',
+        'welcome.enthusiast': 'entusiasta de la tecnología y su impacto en la vida cotidiana, comprometido con la creación de soluciones web innovadoras.',
     }
 } as const;
